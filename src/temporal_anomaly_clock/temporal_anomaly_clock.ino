1315b49430e3a3d1a184ff437877f3f4a802068e
v01dma1n <irek@rybark.com>
2025-10-22 20:34:19 -0400

    gridMap moved to .ino out of the driver

--- src/temporal_anomaly_clock/temporal_anomaly_clock.ino
+++ src/temporal_anomaly_clock/temporal_anomaly_clock.ino
@@ -21,21 +21,36 @@ TemporalAnomalyClockApp& app = TemporalAnomalyClockApp::getInstance();
 // --- Define the data structure and queue for communication ---
 #define DISPLAY_DIGITS 10
 typedef unsigned long DisplayFrame[DISPLAY_DIGITS];
-
 QueueHandle_t frameQueue;
+
+static const unsigned long VFD_GRIDS[] = {
+    0b00000000010000000000, // GRD_01 (Digit 0)
+    0b00000000100000000000, // GRD_02 (Digit 1)
+    0b00000010000000000000, // GRD_03 (Digit 2)
+    0b00010000000000000000, // GRD_04 (Digit 3)
+    0b10000000000000000000, // GRD_05 (Digit 4)
+    0b00000000000000000010, // GRD_06 (Digit 5)
+    0b00000000000000000100, // GRD_07 (Digit 6)
+    0b00000000000000100000, // GRD_08 (Digit 7)
+    0b00000000000100000000, // GRD_09 (Digit 8)
+    0b00000000001000000000  // GRD_10 (Digit 9)
+};
+static const int VFD_DIGIT_COUNT = sizeof(VFD_GRIDS) / sizeof(VFD_GRIDS[0]);
+
 
 /**
  * @brief High-priority task to handle display multiplexing (CONSUMER).
  * Runs on Core 1 (Real-time).
  */
 void displayTask(void *pvParameters) {
-  // sclkPin, misoPin, mosiPin, ssPin, blankPin
+
   DispDriverMAX6921_Hardware hardwareDriver(
       VSPI_SCLK, 
       VSPI_MISO, 
       VSPI_MOSI, 
       VSPI_SS, 
-      VSPI_BLANK
+      VSPI_BLANK,
+      VFD_GRIDS, VFD_DIGIT_COUNT
   );
   hardwareDriver.begin();
 
